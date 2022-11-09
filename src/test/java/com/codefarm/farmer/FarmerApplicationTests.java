c99e04734b04de6dd59734a771be9942e9ad3867
ssw4688 <tmddn4688@gmail.com>
2022-11-09 18:50:09 +0900

    Spring 초기 환경설정

---
+++ src/test/java/com/codefarm/farmer/FarmerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codefarm.farmer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
