23b6a85ef4d1a4252d756c690e53634eac3d7c56
ParkSuiHyeon <dream3773@gmail.com>
2022-11-15 02:08:22 +0900

    박수현 마이페이지 작업 중, 로그인 완료, 회원가입 진행 중

---
+++ src/main/java/com/codefarm/farmer/controller/join/JoinController.java
@@ -0,0 +1,22 @@
+package com.codefarm.farmer.controller.join;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+@RequestMapping("/register/*")
+public class JoinController {
+
+    @GetMapping("")
+    public String joinPage(){
+        return "/join/join";
+    }
+
+    @GetMapping("/registerForm")
+    public String joinFormPage(){
+        return "/join/joinForm";
+    }
+}
