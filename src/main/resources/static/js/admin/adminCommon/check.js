14031e9c2522e91a7e2de539bf6a289487a1e756
xogwan <xogwan@gmail.com>
2022-11-24 13:15:28 +0900

    20221124 연태관 / 관리자페이지 진짜 끝

--- src/main/resources/static/js/admin/adminCommon/check.js
+++ src/main/resources/static/js/admin/adminCommon/check.js
@@ -8,14 +8,6 @@ function outCheck(){
     if(flag) alert('탈퇴 완료');
 }
 
-// 글 수정
-function reviseCheck() {
-    var msg = "글을 수정 하시겠습니까?";
-    var flag = confirm(msg);
-
-    if (flag) alert('수정 완료');
-}
-
 // 글 삭제
 function deleteWriteCheck() {
     var msg = "게시글을 삭제 하시겠습니까?";
@@ -48,12 +40,92 @@ function deleteNoticeCheck() {
     if (flag) alert('삭제 완료');
 }
 
-// 글 등록
-function saveCheck() {
+// 공지글 수정
+function reviseNoticeCheck() {
+    var msg = "글을 수정 하시겠습니까?";
+    var flag = confirm(msg);
+
+    if (flag) {
+        location.href="/adminNotice/notice";
+        alert('수정 완료');
+    }
+}
+
+// 배너글 수정
+function reviseBannerCheck() {
+    var msg = "글을 수정 하시겠습니까?";
+    var flag = confirm(msg);
+
+    if (flag) {
+        location.href="/adminBanner/banner";
+        alert('수정 완료');
+    }
+}
+
+// 농업정보글 수정
+function reviseCropInformationCheck() {
+    var msg = "글을 수정 하시겠습니까?";
+    var flag = confirm(msg);
+
+    if (flag) {
+        location.href="/adminCropInformation/cropInformation";
+        alert('수정 완료');
+    }
+}
+
+// 청년정책글 수정
+function revisePolicyCheck() {
+    var msg = "글을 수정 하시겠습니까?";
+    var flag = confirm(msg);
+
+    if (flag) {
+        location.href="/adminPolicy/policy";
+        alert('수정 완료');
+    }
+}
+
+// 배너글 등록
+function saveBannerCheck() {
     var msg = "글을 등록하시겠습니까?";
     var flag = confirm(msg);
 
-    if (flag) alert('등록 완료');
+    if (flag) {
+        location.href="/adminBanner/banner";
+        alert('등록 완료');
+    }
+}
+
+// 농업정보글 등록
+function saveCropInformationCheck() {
+    var msg = "글을 등록하시겠습니까?";
+    var flag = confirm(msg);
+
+    if (flag) {
+        location.href="/adminCropInformation/cropInformation";
+        alert('등록 완료');
+    }
+}
+
+// 공지글 등록
+function saveNoticeCheck() {
+    var msg = "글을 등록하시겠습니까?";
+    var flag = confirm(msg);
+
+    if (flag) {
+        location.href="/adminNotice/notice";
+        alert('등록 완료');
+    }
+}
+
+// 정책정보글 등록
+function savePolicyCheck() {
+    var msg = "글을 등록하시겠습니까?";
+    var flag = confirm(msg);
+
+    if (flag) {
+        location.href="/adminPolicy/policy";
+        alert('등록 완료');
+    }
 }
 
 // 답변 등록
@@ -61,7 +133,10 @@ function doReplyCheck() {
     var msg = "답변을 등록하시겠습니까?";
     var flag = confirm(msg);
 
-    if (flag) alert('답변 완료');
+    if (flag){
+        location.href="/adminAsk/ask";
+        alert('답변 완료');
+    }
 }
 
 
